b1671db63c5e0f597772ca067dbadd20e95df5ab
Khoi Duong <khoiduongbusiness@gmail.com>
2025-02-07 20:57:39 -0500

    done

--- Lab 2/duongk9_lab_II_code.py
+++ Lab 2/duongk9_lab_II_code.py
@@ -162,7 +162,7 @@ class GraphII:
     def has_cycle(self,):
         visited = set()
         # your implementation for Part 3 goes here
-        def dfs(current, parent):
+        def dfs(current, parent):   # Uses a recursive DFS to check for cycles, when it hits a node in visited, it checks to see if the node is = to itself, it's not then there has to be a cycle.
             visited.add(current)
             for node in self.graph[current]:
                 if node not in visited:
@@ -172,7 +172,7 @@ class GraphII:
                     return True
             return False
         
-        for node in range (len(self.graph)):
+        for node in range (len(self.graph)):    # Perform this for all nodes in a graph, even in disconnected graphs
             if node not in visited:
                 if dfs(node, None):
                     return True
@@ -180,13 +180,16 @@ class GraphII:
     
     def is_connected(self,node1,node2):
         # your implementation for Part 3 goes here
+        
+        # First case we check to see if the two nodes are the same, if they are then of course they are connected
         if node1 == node2:
             return True
 
+        # Keeping track of a visited set, using a set is good here because there can only be one of each item/node in there (no duplicates)
         visited = set([node1])
-        q = deque([node1])
+        q = deque([node1])  # Using a queue for BFS
 
-        while q:
+        while q:    # Basically a BFS approach to find if two edges are connected, returns true if the current node it's examining is equal to the destination node
             current = q.popleft()
             if current == node2:
                 return True
@@ -195,7 +198,7 @@ class GraphII:
                     visited.add(node)
                     q.append(node)
 
-        return False
+        return False # Returns false if it's searched all connected nodes and the previous code did not return true
     
 def BFS_2(graph,src,dst):
     path = []
@@ -208,11 +211,11 @@ def BFS_2(graph,src,dst):
     reached_end = False
     visited = set([src])
 
-    parent = {src: None}
+    parent = {src: None}    # Keeping track of a parent dictionary so we can reconstruct the path again at the end
 
     q = deque([src])
 
-    while q:
+    while q:    # Normal BFS
         current = q.popleft()
 
         if current == dst:
@@ -225,12 +228,12 @@ def BFS_2(graph,src,dst):
                 parent[node] = current
                 q.append(node)
     
-    if reached_end:
+    if reached_end: # Once we've reached the end, we reconstruct the path using the parent dictionary to get a path in reverse order
         node = dst
         while node is not None:
             path.append(node)
             node = parent[node]
-        path.reverse()
+        path.reverse()  # To solve this we then reverse the path to get it to return the correct order
 
     return path
 
@@ -243,14 +246,14 @@ def DFS_2(graph,src,dst):
 
     visited = set()
 
-    def dfs (node):
+    def dfs (node): # Using a recursive approach for this problem, we use the path itself as the stack for DFS
         visited.add(node)
         path.append(node)
 
         if node == dst:
-            return True
+            return True # Check if we have reached the end or not, it returns true so that we know the destination node is connect to the source
 
-        for neighbor in graph.graph[neighbor]:
+        for neighbor in graph.graph[neighbor]:  # Standard implementation of DFS to search for neighbors and adding them into the visited set (marking them)
             if neighbor not in visited:
                 if dfs(neighbor):
                     return True
@@ -259,7 +262,7 @@ def DFS_2(graph,src,dst):
     
     reached_end = dfs(src)
 
-    if reached_end:
+    if reached_end: #Will return a path if the destination node is connected to the source
         return path
     else:
         return []
@@ -268,17 +271,17 @@ def DFS_2(graph,src,dst):
 def BFS_3(graph,src):
     # Your implementation for Part 2 goes here
 
-    predecessors = {}
-
+    predecessors = {}   # Keep a dictionary of a node's predecessors, we use the searched node as the key and the current node that we are searching from as values
+                        # This shows the parent nodes as values and nodes as keys 
     visited = set([src])
     q = deque([src])
 
-    while q:
+    while q:    # Standard BFS implementation here
         current = q.popleft()
         for node in graph[current]:
             if node not in visited:
                 visited.add(node)
-                predecessors[current] = node
+                predecessors[node] = current
                 q.append(node)
                 
     return predecessors
@@ -286,7 +289,7 @@ def BFS_3(graph,src):
 def DFS_3(graph,src):
     # Your implementation for Part 2 goes here
     
-    predecessors = {}
+    predecessors = {}   # Exact same idea as BFS_3 but using DFS with a stack insetad
     visited = set()
     
     def dfs (current, parent):
@@ -336,24 +339,24 @@ def mvc_1(G):
     C = set()
 
     while not is_vertex_cover(G, C):
-        highest_degree = -99999
-        highest_vertex = None
+        highest_degree = -99999 # Set initial highest degree to be a very small number
+        highest_vertex = None   # Set initial highest vertex as none because we have not examined any vertices yet
         for vertex in range(len(G.graph)):
-            degree = len(G.graph[vertex])
-            if degree > highest_degree and vertex not in C:
-                highest_degree = degree
+            degree = len(G.graph[vertex])   # Set the degree as the length of the graph at the specific vertex
+            if degree > highest_degree and vertex not in C: # Checking the highest values and vertex and re-assign them accordingly
+                highest_degree = degree 
                 highest_vertex = vertex
         
         C.add(highest_vertex)
 
-        neighbors = list(G.graph[highest_vertex])
+        neighbors = list(G.graph[highest_vertex])   # Now we want a list of all the neighbors of the vertex
 
         for node in neighbors:
-            G.graph[node].remove(highest_vertex)
+            G.graph[node].remove(highest_vertex)    # Removing the edges that are incident to the highest vertex because they would just cover the same edges as the highest
         
         G.graph[highest_vertex].clear()
             
-    return list(C)
+    return list(C)  
 
 def mvc_2(G):
     # Your implementation for part 6.b goes here
@@ -361,13 +364,13 @@ def mvc_2(G):
     size = len(G.graph)
     nodes = set(range(size))
 
-    while not is_vertex_cover(G, C):
-        remaining = list(nodes - C)
+    while not is_vertex_cover(G, C):    # Checking if our current set creates an MVC
+        remaining = list(nodes - C) # Take away any selected nodes so we don't randomly select a duplicate node
 
         if not remaining:
             break
 
-        node = random.choice(remaining)
+        node = random.choice(remaining) # Pick a random node from the remaining list of nodes
         C.add(node)
 
     return list(C)
@@ -377,13 +380,13 @@ def mvc_3(G):
     C = set()
     e = set()
 
-    for u, neighbors in enumerate(G.graph):
+    for u, neighbors in enumerate(G.graph): # Enumerate here gives us the index of each node which acts as the source and a list of it's neighbors which is the node it's connected to
         for v in neighbors:
-            edge = (min(u,v), max(u,v))
-            e.add(edge)
+            edge = (min(u,v), max(u,v))     # We then build the edge by taking the min value of the two and the max value and put them into a tuple and added to the set e.
+            e.add(edge)                     # The second for loop makes sure we add all edges connected to the node u
     
     while not is_vertex_cover(G, C):
-        u, v = random.choice(list(e))
+        u, v = random.choice(list(e))   # Randomly pick edges from the set e and add the nodes into the set C
         C.add(u)
         C.add(v)
 
@@ -398,12 +401,12 @@ def experiment_1():
     # your implementation for experiment in part 5 goes here
     count = 0
     for _ in range (100):
-        graph = create_random_graph(1000,500) 
+        graph = create_random_graph(1000,500) # Creating 100 random graphs with 1000 nodes and 500 edges.
 
-        if graph.has_cycle():
+        if graph.has_cycle():   # Checking to see if the graph has a cycle or not, if it does then we increment a running total
             count += 1
     
-    proportion = count/100 
+    proportion = count/100 # Divide the count by 100 to find the chance that a randomly generated graph has a cycle
 
     return proportion
 
@@ -431,10 +434,10 @@ def experiment_2():
         mvc2_total = 0
         mvc3_total = 0
         for _ in range (100):
-            graph = create_random_graph(6,edge)
+            graph = create_random_graph(6,edge)     # Creating 100 random graphs of 6 edges and the corresponding edge size given from the edge_sizes list
 
-            optimal_graph = copy.deepcopy(graph)
-            mvc1_graph = copy.deepcopy(graph)
+            optimal_graph = copy.deepcopy(graph)    # Make copies of the original graph to ensure that any changes in the original graph is not reflected on the results of the following functions
+            mvc1_graph = copy.deepcopy(graph)       # It essentially isolates each function to avoid any unexpected errors.
             mvc2_graph = copy.deepcopy(graph)
             mvc3_graph = copy.deepcopy(graph)
 
@@ -443,22 +446,22 @@ def experiment_2():
             mvc2 = mvc_2(mvc2_graph)
             mvc3 = mvc_3(mvc3_graph)
 
-            optimal_total += len(optimal_mvc)
+            optimal_total += len(optimal_mvc)   # Running each MVC algorithm and totalling up their sizes 
             mvc1_total += len(mvc1)
             mvc2_total += len(mvc2)
             mvc3_total += len(mvc3)
 
-        total_optimal_sizes.append(optimal_total)
+        total_optimal_sizes.append(optimal_total)   # Add each of the sizes to the sizes array
         mvc1_sizes.append(mvc1_total)
         mvc2_sizes.append(mvc2_total)
         mvc3_sizes.append(mvc3_total)
     
     for i in range (len(edge_sizes)):
-        mvc1_proportions.append((mvc1_sizes[i]/total_optimal_sizes[i]))
+        mvc1_proportions.append((mvc1_sizes[i]/total_optimal_sizes[i]))     # Then to calculate the proportion we just divide each of the mvc_1,2,3 sizes with the optimal size
         mvc2_proportions.append((mvc2_sizes[i]/total_optimal_sizes[i]))
         mvc3_proportions.append((mvc3_sizes[i]/total_optimal_sizes[i]))
 
-        mvc1_accuracies.append((total_optimal_sizes[i]/mvc1_sizes[i]) * 100)
+        mvc1_accuracies.append((total_optimal_sizes[i]/mvc1_sizes[i]) * 100)    # For accuracy we just flip the calculation of the proportion and multiply by 100 to give us a percentage
         mvc2_accuracies.append((total_optimal_sizes[i]/mvc2_sizes[i]) * 100)
         mvc3_accuracies.append((total_optimal_sizes[i]/mvc3_sizes[i]) * 100)
     
@@ -497,7 +500,7 @@ def experiment_3():
         mvc2_total = 0
         mvc3_total = 0
         for _ in range (100):
-            graph = create_random_graph(node_sizes[i], 15)
+            graph = create_random_graph(node_sizes[i], 15)  # Exact same approach as the previous experiment but we have a list of varying node counts and a constant number of edges instead
 
             optimal_graph = copy.deepcopy(graph)
             mvc1_graph = copy.deepcopy(graph)
@@ -520,7 +523,7 @@ def experiment_3():
         mvc3_sizes.append(mvc3_total)
 
     for i in range (len(node_sizes)):
-        mvc1_proportions.append((mvc1_sizes[i]/total_optimal_sizes[i]))
+        mvc1_proportions.append((mvc1_sizes[i]/total_optimal_sizes[i])) # Calculation of the optimal is the same as the previous experiment
         mvc2_proportions.append((mvc2_sizes[i]/total_optimal_sizes[i]))
         mvc3_proportions.append((mvc3_sizes[i]/total_optimal_sizes[i]))
     
@@ -534,7 +537,7 @@ def experiment_3():
 
     return 0
 
-def plot_experiment1(proportions):
+def plot_experiment1(proportions):  # Function to plot the bar chart for experiment 1, I chose to make a seperate function since the graph for experiments 2 and 3 are quite different to experiment 1
     avg_proportion = np.mean(proportions)
     iterations = len(proportions)
     x = np.arange(iterations) + 1
@@ -553,11 +556,11 @@ def plot_experiment1(proportions):
 
     return
 
-def run_experiment_1(iterations):
+def run_experiment_1(iterations):   # A function to run experiment 1 which runs it for a specific number of iterations
     proportions = []
     for _ in range (iterations):
         res = experiment_1()
-        proportions.append(res)
+        proportions.append(res) # Keeps track of all the proportions of each iteration before plotting the bar chart
     
     
     print(proportions)
